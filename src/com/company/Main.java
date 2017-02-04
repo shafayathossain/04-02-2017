0b7faec5d59e504e906f7b3c34642d0abc5888a2
farhadrahman151 <shafayathossainkhan@gmail.com>
2017-02-04 12:51:57 +0600

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write input
+        int hasmot,opposite,diff;
+        Scanner  input = new Scanner (System.in);
+        hasmot=input.nextInt();
+        opposite=input.nextInt();
+        diff=hasmot-opposite;
+        if(diff>0)
+        {
+
+            System.out.print(diff);
+        }
+        else{
+            System.out.println(diff*-1);
+        }
+
+
+     }
+}
